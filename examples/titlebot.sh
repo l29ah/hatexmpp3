d65c6d59067aaca236353f2e1cee7f70bbefbcf4
Sergey Alirzaev <zl29ah@gmail.com>
2020-02-09 10:42:25 +0300

    + title bot example

---
+++ examples/titlebot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+chatpath=test/mucs/smac@conference.bitcheese.net/__chat
+dd if="$chatpath" iflag=nonblock bs=65536 | while read -r line; do
+url=$(echo "$line" | sed -ne 's#.*\(http[^ ]*\).*#\1#p')
+if [ -n "$url" ]; then
+	title=$(curl -Ls "$url" | sed -ne 's#.*<title>\([^<]*\)<.*#\1#p')
+	echo -n "$title"
+fi
+done >> "$chatpath"
