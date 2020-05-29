27332dff4186729714c7e645b59411e00516ee6f
Sergey Alirzaev <zl29ah@gmail.com>
2020-05-29 18:23:21 +0300

    titlebot: process each URL in a separate process

--- examples/titlebot.sh
+++ examples/titlebot.sh
@@ -3,7 +3,9 @@ chatpath=test/mucs/smac@conference.bitcheese.net/__chat
 dd if="$chatpath" iflag=nonblock bs=65536 | while read -r line; do
 url=$(echo "$line" | sed -ne 's#.*\(http[^ ]*\).*#\1#p')
 if [ -n "$url" ]; then
+	(
 	title=$(curl -Ls "$url" | sed -ne 's#.*<title>\([^<]*\)<.*#\1#p')
-	echo -n "$title"
+	echo -n "$title" >> "$chatpath"
+	) &
 fi
-done >> "$chatpath"
+done
